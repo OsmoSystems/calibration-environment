023215417c687e71ce9d2965d00b574b26cb6442
thatneat <thatneat@gmail.com>
2019-06-27 16:45:15 -0700

    more polish & completeness

--- calibration_environment/drivers/gas_mixer.py
+++ calibration_environment/drivers/gas_mixer.py
@@ -1,21 +1,23 @@
 import logging
+import re
+from typing import Tuple, List, Dict
 
 import pandas as pd
 import serial
 
-""" Controls for Osmo's gas mixer
+""" Controls & monitoring for Osmo's Alicat gas mixing system
 Assumptions:
- * system is one mix controller connected to 2 MFCs
+ * system is one Alicat mix controller connected to 2 MFCs
  * MFC 1 is connected to a nitrogen supply
  * MFC 2 is connected to a supply containing oxygen (likely mixed with nitrogen
 
-Mixer API manuals are stored on our Google Drive:
+Mixer model details and API manuals are stored on our Google Drive:
    https://drive.google.com/drive/u/1/folders/1TFJ5s1ckozGHsIr4bARBcdym7e4Z011Q
    Note that Alicat won't share the full serial API with outsiders, but they have been open to
    providing any specific part of the reference.
 """
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 # Default baud rate - this can be reconfigured
 ALICAT_BAUD_RATE = 19200
@@ -63,6 +65,42 @@ _ONE_BILLION = 1000000000
 _ALICAT_SERIAL_LINE_ENDING = "\r"
 
 
+_MIX_CONTROLLER_STATE_CODES = {
+    0: "Emergency Motion Off (EMO) is active.",
+    1: "Mixing is stopped and cannot be started because of an invalid configuration, usually an invalid mix fraction.",
+    2: "Device is mixing.",
+    3: "Mixing is stopped but can be started when desired.",
+    4: "At least one alarm is active, but external indicators have been quieted.",
+    5: "At least one alarm is active and triggering external indicators.",
+}
+MIX_STATE_CODE_MIXING = 2
+MIX_STATE_CODE_STOPPED_OK = 3
+
+PRESSURE_UNIT_MMHG = 14
+FLOW_UNIT_SLPM = 7
+
+
+class UnexpectedMixerResponse(Exception):
+    # General error for when the mixer doesn't tell us what we expect it to.
+    pass
+
+
+def _assert_mixer_state(actual_response: str, expected_code: int) -> None:
+
+    # Response will look something like "A 3" where "3" is the code
+    match = re.match(r". (\d)", actual_response)
+    actual_code = int(
+        match.groups()[0]  # type: ignore # mypy issue with groups()
+    )
+
+    if actual_code != expected_code:
+        raise UnexpectedMixerResponse(
+            f"Expected mixer status code "
+            f"{expected_code} ('{_MIX_CONTROLLER_STATE_CODES[expected_code]}') "
+            f"but received {actual_code} ('{_MIX_CONTROLLER_STATE_CODES[actual_code]}') "
+        )
+
+
 def send_serial_command_str_and_get_response(
     command_str: str, port: str, timeout: float = 0.06
 ) -> str:
@@ -82,36 +120,38 @@ def send_serial_command_str_and_get_response(
         "{}{}".format(command_str, _ALICAT_SERIAL_LINE_ENDING), encoding="utf8"
     )
 
+    logger.debug(f"Serial command: {command_bytes}")
+
     with serial.Serial(port, ALICAT_BAUD_RATE, timeout=timeout) as connection:
         connection.write(command_bytes)
         # Note: readline() does not do what you would think since the alicat line ending is apparently not normal.
         # We rely on the timeout to find the end of the command.
         return_value_with_line_ending = connection.readline()
 
-    logger.debug("Serial return value:", return_value_with_line_ending)
+    logger.debug(f"Serial response: {return_value_with_line_ending}")
 
     return return_value_with_line_ending.decode("utf8").rstrip(
         _ALICAT_SERIAL_LINE_ENDING
     )
 
 
-def _has_low_feed_pressure(alarm_str):
+def _has_low_feed_pressure(alarm_str: str) -> bool:
     """ Given an alarm string from a mixer status, indicate whether low feed pressure is present
     """
     return bool(int(alarm_str) & _LOW_FEED_PRESSURE_ALARM_BIT)
 
 
-def _ppb_to_fraction(ppb_str):
+def _ppb_to_fraction(ppb_str: str) -> float:
     """ Convert a parts per billion as a string (coming from MFC) to a fraction """
     return int(ppb_str) / _ONE_BILLION
 
 
-def _fraction_to_ppb_str(fraction):
+def _fraction_to_ppb_str(fraction: float) -> str:
     """ Convert a fraction to a parts per billion number suitable for sending over serial """
     return str(int(fraction * _ONE_BILLION))
 
 
-def _parse_flow_fraction(mfc_str: str):
+def _parse_flow_fraction(mfc_str: str) -> float:
     """ Fractions from the MFC come in parts per billion
     However, if there is a communication error or the mixer hasn't been run, """
     if all(character == "-" for character in mfc_str):
@@ -119,18 +159,26 @@ def _parse_flow_fraction(mfc_str: str):
     return _ppb_to_fraction(mfc_str)
 
 
-# TODO:
-#  confirm that units are correctly set on MFC
-#  make sure error handling of malformed/short response isn't too confusing
-#  double-check unit test coverage
-#  add pressure control mode(?) Jacob says this is unlikely
-#  would be nice to get the labels of the gases - this will require separate calls
+def _assert_expected_units(mixer_status_dict: Dict[str, str]) -> None:
+    flow_unit = int(mixer_status_dict["flowUnits"])
+    pressure_unit = int(mixer_status_dict["pressureUnits"])
+    actual_flow_and_pressure_units = flow_unit, pressure_unit
+    expected_flow_and_pressure_units = FLOW_UNIT_SLPM, PRESSURE_UNIT_MMHG
+    if actual_flow_and_pressure_units != expected_flow_and_pressure_units:
+        raise UnexpectedMixerResponse(
+            f"Pressure and flow unit codes {actual_flow_and_pressure_units} "
+            f"are not as expected {expected_flow_and_pressure_units}. "
+            "Please check that mixer is set to our favorite "
+            "units (mmHg for pressuer and SLPM for flow)"
+        )
 
 
-def _parse_mixer_status(mixer_status_str):
+def _parse_mixer_status(mixer_status_str: str) -> pd.Series:
     """ Parse a mixer status string returned from a QMXS ("query mixer status") command """
     mixer_status_values = mixer_status_str.split()
     mixer_status_dict = dict(zip(_MIXER_STATUS_RESPONSE_FIELDS, mixer_status_values))
+
+    _assert_expected_units(mixer_status_dict)
     try:
         return pd.Series(
             {
@@ -154,19 +202,23 @@ def _parse_mixer_status(mixer_status_str):
             }
         )
     except ValueError as e:
-        raise ValueError(
+        raise UnexpectedMixerResponse(
             f"Could not parse response. Response dict was:\n {mixer_status_dict}\n. Error: {str(e)}"
         )
 
 
-def _send_sequence_with_expected_responses(port, command_strs_and_expected_responses):
+def _send_sequence_with_expected_responses(
+    port: str, command_strs_and_expected_responses: List[Tuple[str, str]]
+) -> None:
     for command, expected_response in command_strs_and_expected_responses:
         response = send_serial_command_str_and_get_response(command, port)
-        # TODO: upgrade this to an a more helpful exception
-        assert response == expected_response, (response, expected_response)
+        if response != expected_response:
+            raise UnexpectedMixerResponse(
+                f'Expected mixer response to "{command}" was "{expected_response}" but we got "{response}"'
+            )
 
 
-def get_mixer_status(port):
+def get_mixer_status(port: str) -> pd.Series:
     """ Query mixer status and provide return data helpful for calibration monitoring
 
     Args:
@@ -190,12 +242,85 @@ def get_mixer_status(port):
     # mnemonic: QMXS = "query mixer status"
     command = f"{DEVICE_ID} QMXS"
     response = send_serial_command_str_and_get_response(command, port)
+
+    if not response:
+        raise UnexpectedMixerResponse(
+            f'No response to "{command}" command. Mixer may be disconnected or timeout too short'
+        )
+
     return _parse_mixer_status(response)
+
+
+def _parse_gas_ids(gas_id_response: str) -> pd.Series:
+    """ Gas ID response is something like "A 1 4" where 1 and 4 are the respective gas IDs on the 2 MFCs. """
+    gas_ids = gas_id_response.split()[1:]
+    n2_gas_id, o2_gas_id = map(int, gas_ids)
+
+    return pd.Series({"N2": n2_gas_id, "O2 source gas": o2_gas_id})
+
+
+def get_gas_ids(port: str) -> pd.Series:
+    """ Get IDs of gases on each port.
+    These are not human readable but will allow us to tell when the source gases change -
+    if the mixer is configured with a new, slightly different gas mix, that will get a new number.
+
+    Args:
+        port: Serial port the mix controller is attached to
+
+    Returns:
+        pandas Series of gas IDs: pd.Series({
+            'N2': n2 gas ID,
+            'O2 source gas': o2 source gas ID
+        })
+    """
+
+    command = f"{DEVICE_ID} MXFG"
+    response = send_serial_command_str_and_get_response(command, port)
+
+    if not response:
+        raise UnexpectedMixerResponse(
+            f'No response to "{command}" command. Mixer may be disconnected or timeout too short'
+        )
+
+    return _parse_gas_ids(response)
+
+
+def _assert_valid_mix(flow_rate_slpm: float, o2_source_gas_fraction: float) -> None:
+    """ Check that a given mix is possible on our mixer, raising ValueError if not. """
+    o2_source_gas_max_flow = 2.5
+    n2_max_flow = 10
+    o2_source_gas_target = flow_rate_slpm * o2_source_gas_fraction
+    n2_target = flow_rate_slpm - o2_source_gas_target
+
+    invalid_mix_message = (
+        f"Invalid mix: flow rate {flow_rate_slpm} SLPM, "
+        f"O2 source gas fraction {o2_source_gas_fraction}. "
+    )
+    o2_source_gas_error = (
+        (
+            "O2 source gas mixer only goes up to "
+            f"{o2_source_gas_max_flow} but you are asking for {o2_source_gas_target}. "
+        )
+        if o2_source_gas_target > o2_source_gas_max_flow
+        else ""
+    )
+
+    n2_error = (
+        (
+            f"N2 source gas mixer only goes up to "
+            f"{n2_max_flow} but you are asking for {n2_target}. "
+        )
+        if n2_target > n2_max_flow
+        else ""
+    )
+
+    if o2_source_gas_error or n2_error:
+        raise ValueError(f"{invalid_mix_message}{o2_source_gas_error}{n2_error}")
 
 
 def start_constant_flow_mix(
     port: str, flow_rate_slpm: float, o2_source_gas_fraction: float
-):
+) -> None:
     """ Commands mixer to start a constant flow rate mix
     This also resets any alarms.
 
@@ -210,16 +335,22 @@ def start_constant_flow_mix(
     n2_fraction = 1 - o2_source_gas_fraction
     n2_ppb = _fraction_to_ppb_str(n2_fraction)
     o2_source_gas_ppb = _fraction_to_ppb_str(o2_source_gas_fraction)
+    min_mfc_flow_rate = 2.5  # flow rate of our smallest MFC
+    _assert_valid_mix(flow_rate_slpm, o2_source_gas_fraction)
 
     commands_and_expected_responses = [
         (f"{DEVICE_ID} MXRM 3", "A 3"),  # Set mixer run mode to constant flow
-        (  # Set desired flow rate
-            f"{DEVICE_ID} MXRFF {flow_rate_slpm}",
-            f"{DEVICE_ID} {flow_rate_slpm:.2f} 7 SLPM",
+        (  # Initially set flow rate to a small number to make sure the fraction goes through.
+            f"{DEVICE_ID} MXRFF {min_mfc_flow_rate}",
+            f"{DEVICE_ID} {min_mfc_flow_rate:.2f} {FLOW_UNIT_SLPM} SLPM",
         ),
-        (  # Set target fraction
+        (  # Set target fraction.
             f"{DEVICE_ID} MXMF {n2_ppb}, {o2_source_gas_ppb}",
             f"{DEVICE_ID} {n2_ppb} {o2_source_gas_ppb}",
+        ),
+        (  # Set desired flow rate
+            f"{DEVICE_ID} MXRFF {flow_rate_slpm}",
+            f"{DEVICE_ID} {flow_rate_slpm:.2f} {FLOW_UNIT_SLPM} SLPM",
         ),
         (f"{DEVICE_ID} MXRS 1", f"{DEVICE_ID} 2"),  # mixer run state: Start mixin'
     ]
@@ -227,20 +358,16 @@ def start_constant_flow_mix(
     _send_sequence_with_expected_responses(port, commands_and_expected_responses)
 
 
-"""
-# TODO: gracefully handle error when response code is not 3 because there's an alarm
-0 Emergency Motion Off (EMO) is active.
-1 Mixing is stopped and cannot be started because of an invalid configuration, usually an invalid
-mix fraction.
-2 Device is mixing.
-3 Mixing is stopped but can be started when desired.
-4 At least one alarm is active, but external indicators have been quieted.
-5 At least one alarm is active and triggering external indicators.
-"""
+def stop_flow(port: str) -> None:
+    """ Stop flow on the gas mixer.
 
+    Args:
+        port: serial port that gas mixer is connected on
 
-def stop_flow(port):
+    Returns:
+        None
+    """
     # mnemonic: "MXRS" = "mixer run state"
     command = f"{DEVICE_ID} MXRS 2"
     response = send_serial_command_str_and_get_response(command, port)
-    assert response == f"{DEVICE_ID} 3", response  # Mixer stopped, ready to start
+    _assert_mixer_state(response, MIX_STATE_CODE_STOPPED_OK)
--- calibration_environment/drivers/gas_mixer_test.py
+++ calibration_environment/drivers/gas_mixer_test.py
@@ -1,8 +1,16 @@
+from unittest import mock
+from unittest.mock import sentinel
+
 import math
 import pandas as pd
 import pytest
 
 import calibration_environment.drivers.gas_mixer as module
+
+
+@pytest.fixture
+def mock_send_serial_command_and_get_response(mocker):
+    return mocker.patch.object(module, "send_serial_command_str_and_get_response")
 
 
 @pytest.mark.parametrize(
@@ -34,9 +42,56 @@ class TestPpbConversions:
         assert module._fraction_to_ppb_str(from_mfc) == expected
 
 
-# TODO
-# class TestSendSequenceWithExpectedResponses:
-#     def test_send_sequence_with_expected_responses
+class TestSendSequenceWithExpectedResponses:
+    def test_sends_appropriate_sequence_when_response_is_as_expected(
+        self, mock_send_serial_command_and_get_response
+    ):
+        mock_send_serial_command_and_get_response.side_effect = [
+            sentinel.response_one,
+            sentinel.response_two,
+        ]
+
+        module._send_sequence_with_expected_responses(
+            sentinel.port,
+            [
+                (sentinel.call_one, sentinel.response_one),
+                (sentinel.call_two, sentinel.response_two),
+            ],
+        )
+
+        expected_calls = [
+            mock.call(sentinel.call_one, sentinel.port),
+            mock.call(sentinel.call_two, sentinel.port),
+        ]
+
+        actual_calls = mock_send_serial_command_and_get_response.call_args_list
+        assert actual_calls == expected_calls
+
+    def test_raises_exception_and_stops_further_calls_when_return_value_unexpected(
+        self, mock_send_serial_command_and_get_response
+    ):
+        mock_send_serial_command_and_get_response.side_effect = [
+            sentinel.response_one,
+            sentinel.surprise,  # !
+        ]
+
+        with pytest.raises(module.UnexpectedMixerResponse, match="surprise"):
+            module._send_sequence_with_expected_responses(
+                sentinel.port,
+                [
+                    (sentinel.call_one, sentinel.response_one),
+                    (sentinel.call_two, sentinel.response_two),
+                    (sentinel.call_three, sentinel.response_three),
+                ],
+            )
+
+        expected_calls = [
+            mock.call(sentinel.call_one, sentinel.port),
+            mock.call(sentinel.call_two, sentinel.port),
+        ]
+
+        actual_calls = mock_send_serial_command_and_get_response.call_args_list
+        assert actual_calls == expected_calls
 
 
 class TestParseMixerStatus:
@@ -45,7 +100,7 @@ class TestParseMixerStatus:
         [
             (
                 "Stopped due to N2 low",
-                "A 0 30 2199568 10 7 4 2 Y - -00.00 +00.00 +0001459 ---------- "
+                "A 0 30 2199568 14 7 4 2 Y - -00.00 +00.00 +0001459 ---------- "
                 "2199568 +000.0 +00.00 +921 +1000000000 04096 +018.6 -0.000 +539 +0000000000",
                 pd.Series(
                     {
@@ -61,7 +116,7 @@ class TestParseMixerStatus:
             ),
             (
                 "Running",
-                "A 0 2 4096 10 7 4 2 Y - +00.19 +05.00 +0001463 ---------- "
+                "A 0 2 4096 14 7 4 2 Y - +00.19 +05.00 +0001463 ---------- "
                 "04096 +020.6 +02.50 +923 +0500000000 04096 +017.2 +2.500 +540 +0500000000",
                 pd.Series(
                     {
@@ -77,7 +132,7 @@ class TestParseMixerStatus:
             ),
             (
                 "Stopped - OK",
-                "A 0 6 4096 10 7 4 2 Y - -00.01 +00.00 +0001464 ---------- "
+                "A 0 6 4096 14 7 4 2 Y - -00.01 +00.00 +0001464 ---------- "
                 "04096 +022.7 +00.00 +923 +0000000000 04096 +018.5 +0.000 +541 +1000000000",
                 pd.Series(
                     {
@@ -93,7 +148,7 @@ class TestParseMixerStatus:
             ),
             (
                 "Has not run yet - dashes for fractions",
-                "A 0 6 4096 10 7 4 2 Y - -00.01 +00.00 +0001464 ---------- "
+                "A 0 6 4096 14 7 4 2 Y - -00.01 +00.00 +0001464 ---------- "
                 "04096 +022.7 +00.00 +923 ---------- 04096 +018.5 +0.000 +541 ----------",
                 pd.Series(
                     {
@@ -111,5 +166,115 @@ class TestParseMixerStatus:
     )
     def test_mixer_status(self, name, mixer_status_str, expected):
         actual = module._parse_mixer_status(mixer_status_str)
-        print(actual)
         pd.testing.assert_series_equal(actual, expected)
+
+    @pytest.mark.parametrize(
+        "pressure_and_flow_units_str, should_raise",
+        [("14 7", False), ("14 6", True), ("10 7", True), ("99 99", True)],
+    )
+    def test_blows_up_if_units_incorrectly_set(
+        self, pressure_and_flow_units_str, should_raise
+    ):
+        mixer_status_str = (
+            f"A 0 6 4096 {pressure_and_flow_units_str} 4 2 Y - -00.01 +00.00 +0001464 ---------- "
+            "04096 +022.7 +00.00 +923 ---------- 04096 +018.5 +0.000 +541 ----------"
+        )
+        if should_raise:
+            with pytest.raises(module.UnexpectedMixerResponse, match="units"):
+                module._parse_mixer_status(mixer_status_str)
+        else:
+            # This is more of a meta-test that everything is fine with this test when the units are correct
+            module._parse_mixer_status(mixer_status_str)
+
+
+class TestGetMixerStatus:
+    def test_happy_path(self, mock_send_serial_command_and_get_response, mocker):
+        mock_send_serial_command_and_get_response.return_value = (
+            sentinel.serial_response
+        )
+        mock_parse_mixer_status = mocker.patch.object(
+            module, "_parse_mixer_status", return_value=sentinel.parsed_status
+        )
+
+        status = module.get_mixer_status(sentinel.port)
+
+        mock_send_serial_command_and_get_response.assert_called_with(
+            "A QMXS", sentinel.port
+        )
+        mock_parse_mixer_status.assert_called_with(sentinel.serial_response)
+        assert status == sentinel.parsed_status
+
+    def test_no_response_error(self, mock_send_serial_command_and_get_response, mocker):
+        mock_send_serial_command_and_get_response.return_value = ""
+
+        with pytest.raises(module.UnexpectedMixerResponse, match="No response"):
+            module.get_mixer_status(sentinel.port)
+
+
+class TestParseGasIds:
+    def test_parses_gas_ids(self):
+        expected = pd.Series({"N2": 1, "O2 source gas": 4})
+
+        actual = module._parse_gas_ids("A 1 4")
+
+        pd.testing.assert_series_equal(actual, expected)
+
+
+class TestStopFlow:
+    def test_makes_appropriate_serial_call_and_doesnt_blow_up_in_happy_case(
+        self, mock_send_serial_command_and_get_response
+    ):
+        mock_send_serial_command_and_get_response.return_value = (
+            f"A {module.MIX_STATE_CODE_STOPPED_OK}"
+        )
+
+        module.stop_flow(mock.sentinel.port)
+
+        mock_send_serial_command_and_get_response.assert_called_with(
+            "A MXRS 2", mock.sentinel.port
+        )
+
+    def test_asserts_mixer_state(self, mock_send_serial_command_and_get_response):
+        # Mixer is still mixing after we asked it to stop! This could be any other code
+        mock_send_serial_command_and_get_response.return_value = (
+            f"A {module.MIX_STATE_CODE_MIXING}"
+        )
+
+        with pytest.raises(module.UnexpectedMixerResponse, match="Device is mixing."):
+            module.stop_flow(mock.sentinel.port)
+
+
+class TestAssertMixerState:
+    def test_mixer_state_matches_doesnt_error(self):
+        module._assert_mixer_state("A 3", 3)
+
+    def test_mixer_state_mismatch_provides_helpful_error(self):
+        expected_code = 2  # should be mixing
+        actual_code = 5  # There's an alarm
+        with pytest.raises(module.UnexpectedMixerResponse, match="alarm"):
+            module._assert_mixer_state(f"A {actual_code}", expected_code)
+
+
+class TestAssertValidMix:
+    @pytest.mark.parametrize(
+        "flow_rate_slpm, o2_source_gas_fraction, should_raise",
+        [(5, 0.1, False), (99, 99, True)],
+    )
+    def test_raises_appropriately(
+        self, flow_rate_slpm, o2_source_gas_fraction, should_raise
+    ):
+        if should_raise:
+            with pytest.raises(ValueError, match="mixer only goes up to"):
+                module._assert_valid_mix(flow_rate_slpm, o2_source_gas_fraction)
+        else:
+            module._assert_valid_mix(flow_rate_slpm, o2_source_gas_fraction)
+
+
+class TestStartConstantFlowMix:
+    def smoke_test(self, mocker):
+        mock_send_sequence = mocker.patch.object(
+            module, "_send_sequence_with_expected_responses"
+        )
+
+        module.start_constant_flow_mix(sentinel.port, 5, 0.1)
+        mock_send_sequence.assert_called()

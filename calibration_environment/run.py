7b2efc3b5c44f6aaa1ad7717d44c9b5fcf26b69b
EvanSimpson <evan@osmobot.com>
2019-06-28 08:46:15 -0700

    Update control flow outline

--- calibration_environment/run.py
+++ calibration_environment/run.py
@@ -1,74 +1,105 @@
-# Some mapping between output data column names, and functions to call to fetch them
-# Will live somewhere else
-sensors_configuration = {
-    "sensor_temperature": read_temperature_sensor,
-    "sensor_pressure": read_pressure_sensor,
-}
+import time
+from datetime import datetime
+
+import pandas as pd
+
+from .equilibrate import wait_for_temperature, wait_for_gas_mixer
+from .drivers import gas_mixer, NESLAB_RTE_7
+from .prepare import get_calibration_configuration
+
+
+def get_all_sensor_data_stub(com_port_args):
+    return pd.Series({"data": 1})
+
+
+def get_all_sensor_data(com_port_args):
+    gas_mixer_status = gas_mixer.get_mixer_status(com_port_args["gas_mixer"])
+    gas_ids = gas_mixer.get_gas_ids(com_port_args["gas_mixer"])
+
+    water_bath_status_part_1 = NESLAB_RTE_7.get_status_func_1(
+        com_port_args["water_bath"]
+    )
+    water_bath_status_part_2 = NESLAB_RTE_7.get_status_func_2(
+        com_port_args["water_bath"]
+    )
+
+    return pd.concat(
+        [gas_mixer_status, gas_ids, water_bath_status_part_1, water_bath_status_part_2]
+    )
 
 
 def run(cli_args):
     # Parse the configuration parameters from cli args
     calibration_configuration = get_calibration_configuration(cli_args)
 
-    output_column_names = [
-        "setpoint_temperature",
-        "setpoint_gas_mix",
-        "timestamp",
-        # Maybe also an experiment name / sweep # or other group id
-    ].append(sensors_configuration.keys())
-
-    # Initialize the data and output csv file so it can be `tail`ed
-    collected_data = pd.DataFrame(columns=output_column_names)
-    collected_data.to_csv(calibration_configuration["output_csv"], index=False)
+    write_headers_to_file = True
 
     while True:
         for setpoint in calibration_configuration["setpoints"]:
-            # Set the gax mixer ratio -- still not totally sure on what
-            # the user is inputting, and what the mfc interface is being sent
-            mfc.set_gas_mix(setpoint["mfc"], calibration_configuration["mfc2_gas_pct"])
+            water_bath_com_port = calibration_configuration["com_port_args"][
+                "water_bath"
+            ]
+
+            gas_mixer_com_port = calibration_configuration["com_port_args"]["gas_mixer"]
+
             # Set the water bath temperature
-            water_bath.set_temperature(setpoint["temperature"])
-
+            NESLAB_RTE_7.set_temperature(water_bath_com_port, setpoint["temperature"])
             # Magical function that returns when the system is ready, or has an error
-            error = wait_for_equilibration()
+            wait_for_temperature(water_bath_com_port)
 
-            if error is not None:
-                # Print the error, or wait for input, or perform cleanup and exit
-                pass
+            o2_set_point_fraction = gas_mixer.get_o2_target_fraction(
+                setpoint["flow_rate_slpm"],
+                setpoint["o2_target_gas_fraction"],
+                calibration_configuration["o2_source_gas_fraction"],
+            )
+
+            # Set the gax mixer ratio
+            gas_mixer.start_constant_flow_mix(
+                gas_mixer_com_port, setpoint["flow_rate_slpm"], o2_set_point_fraction
+            )
+
+            wait_for_gas_mixer(gas_mixer_com_port)
 
             for i in range(calibration_configuration["read_count"]):
-                # Set up a data frame to collect data into
-                row = pd.Series(index=output_column_names)
-                row["setpoint_temperature"] = setpoint["temperature"]
-                row["setpoint_gas_mix"] = setpoint["mfc"]
-                row["timestamp"] = datetime.now()
 
-                try:
-                    # Read from each sensor and add to the dataframe
-                    for sensor_name, sensor_read_caller in sensors_configuration:
-                        value = sensor_read_caller()
-                        row[sensor_name] = value
-                except e:
-                    # Handling here might depend on the error?
-                    pass
-
-                # Append the row to the rest of the data
-                collected_data.loc[collected_data.shape[0]] = row
-                # Use `None` as file to return the string value
-                serialized_values = collected_data.iloc[-1].to_csv(
-                    None, index=False, header=False
+                # Use the stub if not using real sensors
+                sensor_data_getter = (
+                    get_all_sensor_data
+                    if calibration_configuration["dry_run"]
+                    else get_all_sensor_data_stub
                 )
 
-                # Append the csv row to the output file
-                with open(calibration_configuration["output_csv"], "w+") as log:
-                    log.write(serialized_values)
+                # Read from each sensor and add to the DataFrame
+                sensor_data = sensor_data_getter(
+                    calibration_configuration["com_port_args"]
+                )
+                row = pd.Series(
+                    {
+                        "setpoint_temperature": setpoint["temperature"],
+                        "setpoint_flow_rate": setpoint["flow_rate_slpm"],
+                        "o2_source_gas_fraction": calibration_configuration[
+                            "o2_source_gas_fraction"
+                        ],
+                        "timestamp": datetime.now(),
+                        **dict(sensor_data),
+                    }
+                )
 
-                # Don't wait to collect the next data point
-                # if we're done collecting at this setpoint
-                if i + 1 != calibration_configuration["read_count"]:
-                    # Wait to collect the next data point
-                    time.sleep(calibration_configuration["read_interval"])
+                # Use mode="a" to append the row to the file
+                pd.DataFrame(row).T.to_csv(
+                    calibration_configuration["output_csv"],
+                    index=False,
+                    header=write_headers_to_file,
+                    mode="a",
+                )
+
+                write_headers_to_file = False
+
+                # Wait to collect the next data point
+                time.sleep(calibration_configuration["collection_wait_time"])
 
         # Not a hard requirement to configurably loop forever
         if not calibration_configuration["loop"]:
+            gas_mixer.stop_flow(gas_mixer_com_port)
+            NESLAB_RTE_7.shutdown(water_bath_com_port)
             return

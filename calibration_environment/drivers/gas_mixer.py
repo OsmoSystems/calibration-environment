9cef6c5ea341e166764a234504cd436d3bb888ac
thatneat <thatneat@users.noreply.github.com>
2019-07-02 14:09:40 -0700

    Addl feedback from PR #3: (#10)
    * _underscores for private things
    * enum type for mix controller state codes - possibly too fancy :P
    * use read_until() instead of readline() on serial interface - allows longer timeout
    * more enums for code sets where we're using more than one value
    * Clarify comments about source gases
    * minor stuff / general polish

--- calibration_environment/drivers/gas_mixer.py
+++ calibration_environment/drivers/gas_mixer.py
@@ -1,6 +1,8 @@
 import logging
 import re
-from typing import Tuple, List, Dict
+from collections import namedtuple
+from enum import IntEnum
+from typing import Tuple, List
 
 import pandas as pd
 import serial
@@ -20,41 +22,54 @@ Mixer model details and API manuals are stored on our Google Drive:
 logger = logging.getLogger(__name__)
 
 # Default baud rate - this can be reconfigured
-ALICAT_BAUD_RATE = 19200
+_ALICAT_BAUD_RATE = 19200
+_ALICAT_SERIAL_LINE_ENDING = "\r"
 
 # The mixer we have is set to device ID "A"
-DEVICE_ID = "A"
+_DEVICE_ID = "A"
 
 # In response to a QMXS (query mixer status) command, the controller provides
-# space-separated values matching these fields:
-_MIXER_STATUS_RESPONSE_FIELDS = [
-    "deviceID",
-    "version",
-    "mixStatus",
-    "mixAlarm",
-    "pressureUnits",
-    "flowUnits",
-    "volumeUnits",
-    "numPorts",
-    "mixAlarmEnable",
-    "gasAnalyzerAlarmEnable",
-    "mixPressure",
-    "mixFlow",
-    "mixVolume",
-    "gasAnalyzer",
-    # The fields after this have the form [name]N where N is an MFC number.
-    # Osmo's mix controller is connected to 2 MFCs so we're expecting that.
-    "status1",
-    "pressure1",
-    "flow1",
-    "totalVolume1",
-    "totalFraction1",
-    "status2",
-    "pressure2",
-    "flow2",
-    "totalVolume2",
-    "totalFraction2",
-]
+# space-separated values matching these fields. Field names are adapted from the alicat serial protocol manual.
+MixerStatusResponse = namedtuple(
+    "MixerStatusResponse",
+    [
+        "device_id",
+        "version",
+        "mix_status",
+        "mix_alarm",
+        "pressure_units",
+        "flow_units",
+        "volume_units",
+        "num_ports",
+        "mix_alarm_enable",
+        "gas_analyzer_alarm_enable",
+        "mix_pressure",
+        "mix_flow",
+        "mix_volume",
+        "gas_analyzer",
+        # In the Alicat manual, the fields after this have the form [name]N where N is an MFC number.
+        # Osmo's mix controller is connected to 2 MFCs.
+        # #1 is for nitrogen and #2 is for the gas that we get oxygen from (referred to as the "O2 source gas").
+        "n2_status",
+        "n2_pressure",
+        "n2_flow",
+        "n2_total_volume",
+        "n2_total_fraction",
+        "o2_source_gas_status",
+        "o2_source_gas_pressure",
+        "o2_source_gas_flow",
+        "o2_source_gas_total_volume",
+        "o2_source_gas_total_fraction",
+    ],
+)
+
+# Expected unit settings
+_PRESSURE_UNIT_CODE_MMHG = 14
+_FLOW_UNIT_CODE_SLPM = 7
+
+# Maximum flow rates on our MFCs, SLPM
+_N2_MAX_FLOW = 10
+_O2_SOURCE_GAS_MAX_FLOW = 2.5
 
 # In status fields, this bit is used to indicate low feed pressure,
 # which generally means a cylinder is exhausted, not connected or there's a kink in the line
@@ -62,22 +77,58 @@ _LOW_FEED_PRESSURE_ALARM_BIT = 0x008000
 
 _ONE_BILLION = 1000000000
 
-_ALICAT_SERIAL_LINE_ENDING = "\r"
+
+class _MixControllerStateCode(IntEnum):
+    """ Codes returned by the mix controller in response to a mixer status (MXRS) command or query """
+
+    emergency_motion_off = 0
+    stopped_configuration_error = 1
+    mixing = 2
+    stopped_ok = 3
+    stopped_error_silent = 4
+    stopped_error = 5
+
+    @property
+    def description(self):
+        return {
+            0: "Emergency Motion Off (EMO) is active.",
+            1: "Mixing is stopped and cannot be started because of an invalid configuration,"
+            " usually an invalid mix fraction.",
+            2: "Device is mixing.",
+            3: "Mixing is stopped but can be started when desired.",
+            4: "At least one alarm is active, but external indicators have been quieted.",
+            5: "At least one alarm is active and triggering external indicators.",
+        }[self]
+
+    def __str__(self):
+        return f'MixControllerState #{self.value}: "{self.description}"'
 
 
-_MIX_CONTROLLER_STATE_CODES = {
-    0: "Emergency Motion Off (EMO) is active.",
-    1: "Mixing is stopped and cannot be started because of an invalid configuration, usually an invalid mix fraction.",
-    2: "Device is mixing.",
-    3: "Mixing is stopped but can be started when desired.",
-    4: "At least one alarm is active, but external indicators have been quieted.",
-    5: "At least one alarm is active and triggering external indicators.",
-}
-MIX_STATE_CODE_MIXING = 2
-MIX_STATE_CODE_STOPPED_OK = 3
+class _MixControllerRunStateRequestCode(IntEnum):
+    """ Codes used to command the mix controller in a mixer status (MXRS) command """
 
-PRESSURE_UNIT_MMHG = 14
-FLOW_UNIT_SLPM = 7
+    clear_alarms_and_start_mixing = 1
+    stop_flow = 2
+    clear_alarms = 3
+    quiet_alarms = 4
+    enter_service_mode = 5
+
+    @property
+    def description(self):
+        return {
+            1: "Clear any existing alarms, exit service mode, and start mixing.",
+            2: "Stop flow. If in service mode, exit service mode and stop flow. Any existing alarms remain active.",
+            3: "Clear any existing alarms. Mixing remains stopped.",
+            4: "Quiet any existing alarm but do not clear any alarms.",
+            5: "Enter service mode. The mix module suspends all communication with the MFCs, "
+            "but any existing alarms remain active.",
+        }[self]
+
+    def __str__(self):
+        return f'MixControllerRunStateRequestCode #{self.value}: "{self.description}"'
+
+
+_MIXER_MODE_CODE_CONSTANT_FLOW = 3
 
 
 class UnexpectedMixerResponse(Exception):
@@ -85,49 +136,47 @@ class UnexpectedMixerResponse(Exception):
     pass
 
 
-def _assert_mixer_state(actual_response: str, expected_code: int) -> None:
+def _assert_mixer_state(
+    actual_response: str, expected_code: _MixControllerStateCode
+) -> None:
 
     # Response will look something like "A 3" where "3" is the code
-    regex = r"{device_id} (\d)".format(device_id=DEVICE_ID)
+    regex = r"{device_id} (\d)".format(device_id=_DEVICE_ID)
     match = re.match(regex, actual_response)
-    actual_code = int(
-        match.groups()[0]  # type: ignore # mypy issue with groups()
+    actual_code = _MixControllerStateCode(
+        int(
+            match.groups()[0]  # type: ignore # mypy issue with groups()
+        )
     )
 
     if actual_code != expected_code:
         raise UnexpectedMixerResponse(
-            f"Expected mixer status code "
-            f"{expected_code} ('{_MIX_CONTROLLER_STATE_CODES[expected_code]}') "
-            f"but received {actual_code} ('{_MIX_CONTROLLER_STATE_CODES[actual_code]}') "
+            f"Expected {expected_code!s} but received {actual_code!s}"
         )
 
 
-def send_serial_command_str_and_get_response(
-    command_str: str, port: str, timeout: float = 0.06
-) -> str:
+def send_serial_command_str_and_get_response(command_str: str, port: str) -> str:
     """ Given a serial command, send it on a serial port and return the response.
     Handles Alicat default serial settings and line endings.
 
     Args:
         command_str: str of command to send, without termination character
         port: serial port to connect to, e.g. COM19 on Windows and /dev/ttyUSB0 on linux
-        timeout: serial timeout to use; the default (0.05s) seems to be plenty.
 
     Returns:
         response, as a string with the alicat end-of-line character (carriage return) stripped
     """
+
     # Add the expected line ending and convert to bytes for serial transmission
-    command_bytes = bytes(
-        "{}{}".format(command_str, _ALICAT_SERIAL_LINE_ENDING), encoding="utf8"
-    )
+    command_bytes = bytes(f"{command_str}{_ALICAT_SERIAL_LINE_ENDING}", encoding="utf8")
 
     logger.debug(f"Serial command: {command_bytes}")
 
-    with serial.Serial(port, ALICAT_BAUD_RATE, timeout=timeout) as connection:
+    with serial.Serial(port, _ALICAT_BAUD_RATE, timeout=0.1) as connection:
         connection.write(command_bytes)
-        # Note: readline() does not do what you would think since the alicat line ending is apparently not normal.
-        # We rely on the timeout to find the end of the command.
-        return_value_with_line_ending = connection.readline()
+        return_value_with_line_ending = connection.read_until(
+            _ALICAT_SERIAL_LINE_ENDING
+        )
 
     logger.debug(f"Serial response: {return_value_with_line_ending}")
 
@@ -162,15 +211,15 @@ def _parse_flow_fraction(mfc_str: str) -> float:
     return _ppb_to_fraction(mfc_str)
 
 
-def _assert_expected_units(mixer_status_dict: Dict[str, str]) -> None:
-    """ Make sure that pressure and flow units configured on the mixer corrspond to mmHg and SLPM, respectively
+def _assert_expected_units(mixer_status_response: MixerStatusResponse) -> None:
+    """ Make sure that pressure and flow units configured on the mixer correspond to mmHg and SLPM, respectively
     to prevent misinterpretation of results.
     """
-    pressure_unit = int(mixer_status_dict["pressureUnits"])
-    flow_unit = int(mixer_status_dict["flowUnits"])
+    pressure_unit = int(mixer_status_response.pressure_units)
+    flow_unit = int(mixer_status_response.flow_units)
 
     actual_pressure_and_flow_units = pressure_unit, flow_unit
-    expected_pressure_and_flow_units = PRESSURE_UNIT_MMHG, FLOW_UNIT_SLPM
+    expected_pressure_and_flow_units = _PRESSURE_UNIT_CODE_MMHG, _FLOW_UNIT_CODE_SLPM
 
     if actual_pressure_and_flow_units != expected_pressure_and_flow_units:
         raise UnexpectedMixerResponse(
@@ -184,34 +233,34 @@ def _assert_expected_units(mixer_status_dict: Dict[str, str]) -> None:
 def _parse_mixer_status(mixer_status_str: str) -> pd.Series:
     """ Parse a mixer status string returned from a QMXS ("query mixer status") command """
     mixer_status_values = mixer_status_str.split()
-    mixer_status_dict = dict(zip(_MIXER_STATUS_RESPONSE_FIELDS, mixer_status_values))
+    mixer_status_response = MixerStatusResponse(*mixer_status_values)
 
-    _assert_expected_units(mixer_status_dict)
+    _assert_expected_units(mixer_status_response)
     try:
         return pd.Series(
             {
-                "flow rate (SLPM)": float(mixer_status_dict["mixFlow"]),
-                "mix pressure (mmHg)": float(mixer_status_dict["mixPressure"]),
+                "flow rate (SLPM)": float(mixer_status_response.mix_flow),
+                "mix pressure (mmHg)": float(mixer_status_response.mix_pressure),
                 "low feed pressure alarm": _has_low_feed_pressure(
-                    mixer_status_dict["mixAlarm"]
+                    mixer_status_response.mix_alarm
                 ),
                 "low feed pressure alarm - N2": _has_low_feed_pressure(
-                    mixer_status_dict["status1"]
+                    mixer_status_response.n2_status
                 ),
                 "low feed pressure alarm - O2 source gas": _has_low_feed_pressure(
-                    mixer_status_dict["status2"]
+                    mixer_status_response.o2_source_gas_status
                 ),
                 "N2 fraction in mix": _parse_flow_fraction(
-                    mixer_status_dict["totalFraction1"]
+                    mixer_status_response.n2_total_fraction
                 ),
                 "O2 source gas fraction in mix": _parse_flow_fraction(
-                    mixer_status_dict["totalFraction2"]
+                    mixer_status_response.o2_source_gas_total_fraction
                 ),
             }
         )
     except ValueError as e:
         raise UnexpectedMixerResponse(
-            f"Could not parse response. Response dict was:\n {mixer_status_dict}\n. Error: {str(e)}"
+            f"Could not parse response. Response was:\n {mixer_status_response}\n. Error: {str(e)}"
         )
 
 
@@ -241,16 +290,17 @@ def get_mixer_status(port: str) -> pd.Series:
             low feed pressure alarm: whether any,
             low feed pressure alarm - N2: feed pressure alarm specific to N2 input,
             low feed pressure alarm - O2 source gas: feed pressure alarm specific to O2 source gas input,
-            N2 fraction in mix: actual fraction of the N2 source gas, in the mix.
+            N2 fraction in mix: Fraction of the mix that comes from the N2 source gas canister.
                 NOTE that this is not necessarily the fractional volume of nitrogen in the result, since
                 the O2 source gas may contain nitrogen as well.
-            O2 source gas fraction in mix: 1,
+            O2 source gas fraction in mix: Fraction of the mix that comes from
+                the O2 source gas (the canister containing oxygen)
                 NOTE that this is not necessarily the fractional volume of O2 in the result, since
                 the O2 source gas may not be pure oxygen if we are using a premixed source gas.
 
     """
     # mnemonic: QMXS = "query mixer status"
-    command = f"{DEVICE_ID} QMXS"
+    command = f"{_DEVICE_ID} QMXS"
     response = send_serial_command_str_and_get_response(command, port)
 
     if not response:
@@ -283,8 +333,8 @@ def get_gas_ids(port: str) -> pd.Series:
             'O2 source gas': o2 source gas ID
         })
     """
-
-    command = f"{DEVICE_ID} MXFG"
+    # mnemonic: "MXFG" = "mixer feed gases"
+    command = f"{_DEVICE_ID} MXFG"
     response = send_serial_command_str_and_get_response(command, port)
 
     if not response:
@@ -300,31 +350,29 @@ def _assert_valid_mix(flow_rate_slpm: float, o2_source_gas_fraction: float) -> N
     Raises:
         ValueError if the target flow rate and fraction are not achievable by the mixer configuration.
     """
-    o2_source_gas_max_flow = 2.5
-    n2_max_flow = 10
     o2_source_gas_target = flow_rate_slpm * o2_source_gas_fraction
     n2_target = flow_rate_slpm - o2_source_gas_target
 
     invalid_mix_message = (
-        f"Invalid mix: flow rate {flow_rate_slpm} SLPM, "
+        f"Invalid mix requested: flow rate {flow_rate_slpm} SLPM, "
         f"O2 source gas fraction {o2_source_gas_fraction}. "
     )
 
     o2_source_gas_error = (
         (
             "O2 source gas mixer only goes up to "
-            f"{o2_source_gas_max_flow} but {o2_source_gas_target} is required for desired mix. "
+            f"{_O2_SOURCE_GAS_MAX_FLOW} but {o2_source_gas_target} is required for desired mix. "
         )
-        if o2_source_gas_target > o2_source_gas_max_flow
+        if o2_source_gas_target > _O2_SOURCE_GAS_MAX_FLOW
         else ""
     )
 
     n2_error = (
         (
             f"N2 source gas mixer only goes up to "
-            f"{n2_max_flow} but {n2_target} is required for desired mix. "
+            f"{_N2_MAX_FLOW} but {n2_target} is required for desired mix. "
         )
-        if n2_target > n2_max_flow
+        if n2_target > _N2_MAX_FLOW
         else ""
     )
 
@@ -371,24 +419,32 @@ def start_constant_flow_mix(
     n2_fraction = 1 - target_o2_source_gas_fraction
     n2_ppb = _fraction_to_ppb_str(n2_fraction)
     o2_source_gas_ppb = _fraction_to_ppb_str(target_o2_source_gas_fraction)
-    min_mfc_flow_rate = 2.5  # flow rate of our smallest MFC
+    min_mfc_flow_rate = min(
+        _O2_SOURCE_GAS_MAX_FLOW, _N2_MAX_FLOW
+    )  # flow rate of our smallest MFC
     _assert_valid_mix(target_flow_rate_slpm, target_o2_source_gas_fraction)
 
     commands_and_expected_responses = [
-        (f"{DEVICE_ID} MXRM 3", "A 3"),  # Set mixer run mode to constant flow
+        (  # Set mixer run mode to constant flow
+            f"{_DEVICE_ID} MXRM {_MIXER_MODE_CODE_CONSTANT_FLOW}",
+            f"A {_MIXER_MODE_CODE_CONSTANT_FLOW}",
+        ),
         (  # Initially set flow rate to a small number to make sure the fraction goes through.
-            f"{DEVICE_ID} MXRFF {min_mfc_flow_rate}",
-            f"{DEVICE_ID} {min_mfc_flow_rate:.2f} {FLOW_UNIT_SLPM} SLPM",
+            f"{_DEVICE_ID} MXRFF {min_mfc_flow_rate}",
+            f"{_DEVICE_ID} {min_mfc_flow_rate:.2f} {_FLOW_UNIT_CODE_SLPM} SLPM",
         ),
         (  # Set target fraction.
-            f"{DEVICE_ID} MXMF {n2_ppb}, {o2_source_gas_ppb}",
-            f"{DEVICE_ID} {n2_ppb} {o2_source_gas_ppb}",
+            f"{_DEVICE_ID} MXMF {n2_ppb}, {o2_source_gas_ppb}",
+            f"{_DEVICE_ID} {n2_ppb} {o2_source_gas_ppb}",
         ),
         (  # Set desired flow rate
-            f"{DEVICE_ID} MXRFF {target_flow_rate_slpm}",
-            f"{DEVICE_ID} {target_flow_rate_slpm:.2f} {FLOW_UNIT_SLPM} SLPM",
+            f"{_DEVICE_ID} MXRFF {target_flow_rate_slpm}",
+            f"{_DEVICE_ID} {target_flow_rate_slpm:.2f} {_FLOW_UNIT_CODE_SLPM} SLPM",
         ),
-        (f"{DEVICE_ID} MXRS 1", f"{DEVICE_ID} 2"),  # mixer run state: Start mixin'
+        (  # mixer run state: Start mixin'
+            f"{_DEVICE_ID} MXRS {_MixControllerRunStateRequestCode.clear_alarms_and_start_mixing.value}",
+            f"{_DEVICE_ID} {_MixControllerStateCode.mixing.value}",
+        ),
     ]
 
     _send_sequence_with_expected_responses(port, commands_and_expected_responses)
@@ -408,6 +464,6 @@ def stop_flow(port: str) -> None:
             Likely cause is that the mixer was already stopped due to an alarm.
     """
     # mnemonic: "MXRS" = "mixer run state"
-    command = f"{DEVICE_ID} MXRS 2"
+    command = f"{_DEVICE_ID} MXRS {_MixControllerRunStateRequestCode.stop_flow.value}"
     response = send_serial_command_str_and_get_response(command, port)
-    _assert_mixer_state(response, MIX_STATE_CODE_STOPPED_OK)
+    _assert_mixer_state(response, _MixControllerStateCode.stopped_ok)

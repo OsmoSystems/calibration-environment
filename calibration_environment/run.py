e239a369bed5655c6dbdc6079b83d4d394ea8fae
thatneat <thatneat@gmail.com>
2019-07-16 18:45:19 -0700

    ehhh, let's not do that

--- calibration_environment/run.py
+++ calibration_environment/run.py
@@ -114,7 +114,6 @@ def run(cli_args=None):
 
         for _, setpoint in calibration_configuration.setpoints.iterrows():
 
-            logging.info(f"Setting setpoint: {setpoint}")
             water_bath.send_command_and_parse_response(
                 water_bath_com_port,
                 command_name="Set Setpoint",

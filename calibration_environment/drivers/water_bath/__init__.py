c2e056c925ed1b780b54e2575ec78ded50345e80
thatneat <thatneat@gmail.com>
2019-07-25 16:41:13 -0700

    whitespace

--- calibration_environment/drivers/water_bath/__init__.py
+++ calibration_environment/drivers/water_bath/__init__.py
@@ -13,5 +13,4 @@ A driver for the Thermo Scientific NESLAB RTE 17 Temperature-controlled water ba
 
 Excerpts from the datasheet:
 https://drive.google.com/file/d/1Tg-e1C8Ht8BE7AYzKVSqjw9bhWWxqKlz?disco=AAAADMVYeIw
-
 """

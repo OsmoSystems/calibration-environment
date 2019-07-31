4d312e61a42661252bc1e17899dca9f64f5206c8
Michael Silver <rush340@gmail.com>
2019-07-31 13:26:36 -0700

    add docstring and type hints

--- calibration_environment/equilibrate.py
+++ calibration_environment/equilibrate.py
@@ -21,7 +21,26 @@ _YSI_DO_MMHG_FIELD_NAME = "YSI DO (mmHg)"
 _TIMESTAMP_FIELD_NAME = "timestamp"
 
 
-def _is_field_equilibrated(sensor_data_log, field_name, max_variation, min_stable_time):
+def _is_field_equilibrated(
+    sensor_data_log: pd.DataFrame,
+    field_name: str,
+    max_variation: float,
+    min_stable_time: datetime.timedelta,
+):
+    """Determines whether the data in field_name of sensor_data_log has equilibrated.
+
+    Args:
+        sensor_data_log: pd.DataFrame containing rows of values returned from collect_data_to_csv
+        field_name: name of the column in sensor_data_log to read for determining equilibration
+        max_variation: the maximum difference between the min and max in the last min_stable_time
+            of data for equilibration
+        min_stable_time: the minimum amount of time to have been stable (according to max_variation)
+            for equilibration
+
+    Returns:
+        True if the field's values have equilbrated
+        False if they have not equilibrated or if we don't have at least min_stable_time of data
+    """
     oldest_timestamp = sensor_data_log[_TIMESTAMP_FIELD_NAME].min()
     newest_timestamp = sensor_data_log[_TIMESTAMP_FIELD_NAME].max()
 
@@ -43,13 +62,13 @@ def _is_field_equilibrated(sensor_data_log, field_name, max_variation, min_stabl
 
 
 def _wait_for_equilibration(
-    setpoint,
-    calibration_configuration,
-    loop_count,
-    equilibration_status,
-    field_name,
-    max_variation,
-    min_stable_time,
+    setpoint: pd.Series,
+    calibration_configuration: CalibrationConfiguration,
+    loop_count: int,
+    equilibration_status: EquilibrationStatus,
+    field_name: str,
+    max_variation: float,
+    min_stable_time: datetime.timedelta,
 ):
     sensor_data_log = pd.DataFrame()
 

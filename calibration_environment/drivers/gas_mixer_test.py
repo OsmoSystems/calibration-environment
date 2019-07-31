35128d7c71c053fedce0eb2c7eea1427bed856ba
thatneat <thatneat@gmail.com>
2019-07-31 13:55:06 -0700

    stop_flow should not blow up if flow is stopped, regardless of error status

--- calibration_environment/drivers/gas_mixer_test.py
+++ calibration_environment/drivers/gas_mixer_test.py
@@ -330,10 +330,10 @@ class TestStopFlow:
 
 class TestAssertMixerState:
     def test_mixer_state_matches_doesnt_error(self):
-        module._assert_mixer_state("A 3", module._MixControllerStateCode(3))
+        module._assert_mixer_state("A 3", [module._MixControllerStateCode(3)])
 
     def test_mixer_state_mismatch_provides_helpful_error(self):
-        expected_code = module._MixControllerStateCode(3)  # should be mixing
+        expected_code = [module._MixControllerStateCode(3)]  # should be mixing
         actual_code_number = 5  # There's an alarm
         with pytest.raises(module.UnexpectedMixerResponse, match="alarm"):
             module._assert_mixer_state(f"A {actual_code_number}", expected_code)
